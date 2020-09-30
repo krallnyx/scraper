03016c58f489100e8bd73558365e83123926cc44
KrallNyx <arnaud@jeanroch.fr>
2020-09-30 17:27:52 +0100

    Removing debug print

--- main.py
+++ main.py
@@ -67,17 +67,6 @@ class scrap():
                     category,
                     review_rating,
                     image_url])
-        print([product_page_url,
-                    universal_product_code,
-                    title,
-                    price_including_tax,
-                    price_excluding_tax,
-                    number_available,
-                    product_description,
-                    category,
-                    review_rating,
-                    image_url])
-
 
     # writing the headers in the CSV file
     with open('books.csv', 'w', newline='',encoding="utf-8") as csvfile:
